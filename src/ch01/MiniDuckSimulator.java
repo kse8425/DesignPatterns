923c1e7e62a2dd1bce77b09e122f76b17e1844de
kse8425 <kse8425z@gmail.com>
2022-05-19 10:42:03 +0900

    동적으로 행동 지정하기

--- src/ch01/MiniDuckSimulator.java
+++ src/ch01/MiniDuckSimulator.java
@@ -2,12 +2,28 @@ package ch01;
 
 public class MiniDuckSimulator {
     public static void main(String[] args){
-        Duck mallardDuck = new MallardDuck();
-        mallardDuck.display();      // 저는 물오리입니다.
-        mallardDuck.swim();         // 모든 오리는 물에 뜹니다. 가짜 오리도 뜨죠
-        mallardDuck.performFly();   // 날고 있어요
-        mallardDuck.performQuack(); // 꽥
+        System.out.println("================================");
+        Duck mallard = new MallardDuck();
+        mallard.display();      // 저는 물오리입니다.
+        mallard.swim();         // 모든 오리는 물에 뜹니다. 가짜 오리도 뜨죠
+        mallard.performQuack(); // 꽥
+        mallard.performFly();   // 날고 있어요
+        System.out.println("================================");
+
+        System.out.println();
+
+        System.out.println("================================");
+        Duck model = new ModelDuck();
+        model.display();        // 저는 모형 오리입니다.
+        model.swim();         // 모든 오리는 물에 뜹니다. 가짜 오리도 뜨죠
+        model.performQuack();   // 꽥
+        model.performFly();     // 저는 못 날아요
+
+        System.out.println("========= 동적 행동 지정 후 ===========");
+        model.setQuackBehavior(new Squeck());
+        model.setFlyBehavior(new FlyRocketPowered());
+        model.performQuack();   // 삑
+        model.performFly();     // 날고 있어요
+        System.out.println("================================");
     }
-
-
 }
